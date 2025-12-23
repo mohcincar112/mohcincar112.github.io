5c0423f1b2566bda53372f3aebcf21eb47c347bc
mohcincar112 <mohcincar@gmail.com>
2025-12-24 00:01:38 +0100

    Imagenes + estilos

---
+++ script.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('section').forEach(section => {
+  section.classList.add('hidden');
+  observer.observe(section);
+});
